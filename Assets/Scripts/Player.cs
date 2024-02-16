e4009938eae4b1fc89d574c233d0936debc8952f
VicCTS <victordm.cts@gmail.com>
2024-02-16 08:46:30 +0100

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Transform gunPosition;
+    [SerializeField] int bulletType = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = PoolManager.Instance.GetPooledObjects(bulletType, gunPosition.position, gunPosition.rotation);
+
+            if(bullet != null)
+            {
+                bullet.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("Pool demasiado pequeño");
+            }
+            
+        }
+    }
+}
